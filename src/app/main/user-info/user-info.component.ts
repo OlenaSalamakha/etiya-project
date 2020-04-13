14c9d124a0d5b9ce5abf60e4a5a4fc9762af6b2f
Olena <o.lenaa8999@gmail.com>
2020-04-13 18:53:51 +0300

    created search for users

--- src/app/main/user-info/user-info.component.ts
+++ src/app/main/user-info/user-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserData } from 'src/app/shared/models/user-data.model';
 import { map } from 'rxjs/operators';
 import { DataStorageService } from 'src/app/shared/services/data-storage.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
@@ -10,19 +11,37 @@ import { DataStorageService } from 'src/app/shared/services/data-storage.service
 export class UserInfoComponent implements OnInit {
   loadedUsers: UserData[] = [];
   isFetching = false;
-  loadedAddress: string[];
-  searchText;
+  searchform: FormGroup;
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.searchform = this.fb.group({
+      firstname: null,
+      lastname: null,
+      uname: null,
+      phone: null,
+      email: null
+    });
     this.onFetchData();
   }
 
-  onFind() {
-    for (let user of this.loadedUsers) {
-      return this.loadedAddress = user.additionalAddress;
-    }
+  getUserValue(fieldname: string) {
+    return this.searchform.get(fieldname).value;
+  }
+
+  onFound() {
+    this.loadedUsers.forEach(user => console.log(user.id));
+    this.searchUser('firstname', 'firstname');
+    this.searchUser('lastname', 'lastname');
+    this.searchUser('uname', 'username');
+    this.searchUser('email', 'email');
+    this.searchUser('phone', 'phone');
+  }
+
+  onClear() {
+    this.searchform.reset();
+    this.onFetchData();
   }
 
   onFetchData() {
@@ -44,5 +63,15 @@ export class UserInfoComponent implements OnInit {
         this.isFetching = false;
         this.loadedUsers = userData;
       });
+  }
+
+  searchUser(fieldname: string, key: string) {
+    if (this.getUserValue(fieldname)) {
+      this.loadedUsers = this.loadedUsers.filter(user => {
+        if (user[key].includes(this.searchform.get(fieldname).value)) {
+          return user[key];
+        }
+      });
+    }
   }
 }
